ba50c09f1b1381b0abaa11713a2af2fb68f9acf2
agusacost <juanagustinacost@gmail.com>
2024-06-05 12:05:55 -0300

    styles added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,14 +20,52 @@ export default function Home() {
         setShortUrl(data.shortUrl);
       });
   };
+
+  const handleCopy = () => {
+    if (shortUrl) {
+      navigator.clipboard
+        .writeText("http://localhost:3000/shortUrl/" + shortUrl)
+        .then();
+    }
+  };
+
   return (
-    <div>
-      <h1>Url shortener</h1>
-      <form onSubmit={handleSubmit}>
-        <input type="text" ref={inputRef} placeholder="url" />
-        <button>Acortar</button>
-      </form>
-      <span className="">{shortUrl}</span>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900">
+      <h1 className="text-6xl font-semibold mb-10 text-blue-500">Short URL</h1>
+      <div className="border-2 border-blue-500 rounded-md shadow-sm p-24 flex flex-col items-center justify-center">
+        <div className="mb-10">
+          <h2 className="font-semibold text-2xl text-blue-400">
+            Paste your URL to be shortened
+          </h2>
+        </div>
+        <form onSubmit={handleSubmit} className="flex items-center mb-4">
+          <input
+            type="text"
+            ref={inputRef}
+            placeholder="Paste your URL here"
+            className="shadow appearance-none border-2  w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          />
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-8 focus:outline-none focus:shadow-outline">
+            Shorten
+          </button>
+        </form>
+        <div>
+          {shortUrl && (
+            <div className="text-center mt-4">
+              <span
+                className="text-blue-300 font-bold cursor-pointer"
+                onClick={handleCopy}
+              >
+                Short URL: http://localhost:3000/shortUrl/{shortUrl}
+              </span>
+              <br />
+              <span className="text-slate-400 text-sm">
+                *Click on link to copy on clipboard
+              </span>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
